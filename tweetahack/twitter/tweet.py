0a16e8ef66112c136958daadb3bdf8b2bfd30753
Nick <love2jn@dukes.jmu.edu>
2016-04-02 00:28:10 -0400

    added functionality to reply to a group of tweets with one message

--- tweetahack/twitter/tweet.py
+++ tweetahack/twitter/tweet.py
@@ -1,12 +1,14 @@
 import tweepy
 from init_tweepy import InitTweepy
-
+from mentions    import Mentions
 class Tweet:
     def __init__(self):
-        self.api = InitTweepy().get_api()
+        self.api      = InitTweepy().get_api()
+        self.mentions = Mentions() 
 
     def post_tweet(self, tweet):
         self.api.update_status(status=tweet)
 
-    def reply_to_tweet(self, tweet, status):
-        self.api.update_status(status="@"+tweet.user.name+" "+status, in_reply_to_status_id=tweet.id)
+    def reply_to_tweets(self, tweets, status):
+        for tweet in tweets:
+            self.api.update_status(status="@"+tweet.user.screen_name+" "+status, in_reply_to_status_id=tweet.id)
