1fa1353268a78a5b5808098863f7299693fc5051
themailman05 <themailman05@gmail.com>
2016-04-02 08:37:09 +0000

    Machine learninggit add learn.py requirements.txt !

--- learn.py
+++ learn.py
@@ -1,4 +1,5 @@
 import praw
+import pickle
 from textblob.classifiers import NaiveBayesClassifier
 
 UA = 'Trying to learn about food'
@@ -9,8 +10,8 @@ train = []
 test = []
 count = 0
 
-for submission in subreddit.get_hot(limit=100):
-	if count <= 80:
+for submission in subreddit.get_hot(limit=200):
+	if count <= 150:
 		if str(subreddit) in subreddit.url and submission.selftext[:2000]:
 			train.append((submission.selftext[:2000], 'food'))
 	else:
@@ -21,8 +22,8 @@ for submission in subreddit.get_hot(limit=100):
 subreddit = r.get_subreddit('lifehacks')
 count = 0
 
-for submission in subreddit.get_hot(limit=100):
-	if count <= 80:
+for submission in subreddit.get_hot(limit=200):
+	if count <= 150:
 		if str(subreddit) in subreddit.url and submission.selftext[:2000]:
 			train.append((submission.selftext[:2000], 'life'))
 	else:
@@ -30,5 +31,34 @@ for submission in subreddit.get_hot(limit=100):
 			test.append((submission.selftext[:2000], 'life'))
 	count += 1
 
+subreddit = r.get_subreddit('GetStudying')
+count = 0
+
+for submission in subreddit.get_hot(limit=200):
+	if count <= 150:
+		if str(subreddit) in subreddit.url and submission.selftext[:2000]:
+			train.append((submission.selftext[:2000], 'study'))
+	else:
+		if str(subreddit) in subreddit.url and submission.selftext[:2000]:
+			test.append((submission.selftext[:2000], 'study'))
+	count += 1
+
+subreddit = r.get_subreddit('outdoor')
+count = 0
+
+for submission in subreddit.get_hot(limit=200):
+	if count <= 150:
+		if str(subreddit) in subreddit.url and submission.selftext[:2000]:
+			train.append((submission.selftext[:2000], 'events'))
+	else:
+		if str(subreddit) in subreddit.url and submission.selftext[:2000]:
+			test.append((submission.selftext[:2000], 'events'))
+	count += 1
+
+f = open('myclassifier.pickle', 'wb')
 cl = NaiveBayesClassifier(train)
+pickle.dump(cl, f)
+f.close()
 print str(cl.accuracy(test) * 100) + "%"
+cl.show_informative_features(10)
+
