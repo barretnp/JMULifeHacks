a07ea9e9ba86d654ba237487ce897473f68c0325
Nick <love2jn@dukes.jmu.edu>
2016-04-01 23:05:30 -0400

    mentions with some previous mention functionality

--- tweetahack/mentions.py
+++ tweetahack/mentions.py
@@ -12,5 +12,18 @@ class Mentions:
 
         self.api = tweepy.API(auth)
 
-    def get_mentions(self, cnt=1, since=None):
-        return self.api.mentions_timeline(since_id=since, count=cnt)
+    def get_mentions(self, count=1, since=None):
+        return self.api.mentions_timeline(since_id=since, count=count)
+
+    def get_mention_id(self):
+        return self.api.mentions_timeline(count=1)[0].id
+
+    def process_nprevious(self, max_id, count=1):
+        return self.api.mentions_timeline(max_id=(max_id-1), count=count)
+
+    #def process_allprevious(self, max_id, rate=None):
+     #   if len(process_nprevious(max_id)) > 0:
+      #      return [].extend(process_allprevious())
+       # else:
+        #    return []
+            
