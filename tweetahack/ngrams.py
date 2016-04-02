9b6d386f8308ee55567d6c7377e1280ffed36665
themailman05 <themailman05@gmail.com>
2016-04-02 07:57:07 +0000

    stopping pt

--- tweetahack/ngrams.py
+++ tweetahack/ngrams.py
@@ -1,12 +1,13 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.util import ngrams
-from nltk.tokenize.api import StringTokenizer
+from nltk.tokenize import TweetTokenizer
 import nltk.data
 from database import db_session
 from models import HackCorpus
 import cPickle
 from itertools import chain, imap
 import collections
+import random
 
 def flatten(l):
     for el in l:
@@ -16,26 +17,67 @@ def flatten(l):
         else:
 	    yield el
 
-def generate_ngrams(d_session):
-    corpi = [post.text for post in d_session.query(HackCorpus).filter(HackCorpus.text != "").all()]
-    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+def get_corpus(d_session, category=None, size=None):
+    """
+    generate a corpus of traning text from DB
+    """
+    corpi = [post.text for post in d_session.query(HackCorpus).filter(HackCorpus.text != "" and HackCorpus.category=='lifehacks').all()]
     bigtext = "\n".join(corpi)
-    #sentences = [sentence for sentence in sent_detector.tokenize(bigtext)]
-    #print sentences[5]
-    gen_ngrams = ngrams(bigtext.split(' '), 2)
-    ngram_dict = {}
-    for item in gen_ngrams:
-        if item not in ngram_dict:
-            ngram_dict[item]=1
-        else:
-            ngram_dict[item]+=1
-    return ngram_dict
+    return bigtext
+
+def tokenize(big_text):
+    tknzer = TweetTokenizer()
+    tokens = tknzer.tokenize(big_text)
+    return tokens
+
+def build_model(tokens, n):
+  "Builds a Markov model from the list of tokens, using n-grams of length n."
+  model = dict()
+  if len(tokens) < n:
+    return model
+  for i in range(len(tokens) - n):
+    gram = tuple(tokens[i:i+n])
+    next_token = tokens[i+n]
+    if gram in model:
+      model[gram].append(next_token)
+    else:
+      model[gram] = [next_token]
+  final_gram = tuple(tokens[len(tokens)-n:])
+  if final_gram in model:
+    model[final_gram].append(None)
+  else:
+    model[final_gram] = [None]
+  return model
+
+def generate(model, n, seed=None, max_iterations=100):
+  """Generates a list of tokens from information in model, using n as the
+    length of n-grams in the model. Starts the generation with the n-gram
+    given as seed. If more than max_iteration iterations are reached, the
+    process is stopped. (This is to prevent infinite loops)"""
+  if seed is None:
+    seed = random.choice(model.keys())
+  output = list(seed)
+  current = tuple(seed)
+  for i in range(max_iterations):
+    if current in model:
+      possible_next_tokens = model[current]
+      next_token = random.choice(possible_next_tokens)
+      if next_token is None: break
+      output.append(next_token)
+      current = tuple(output[-n:])
+    else:
+      break
+  return output
+
 
 
 def main():
-    ngrams_generated = generate_ngrams(db_session)
-    for key in ngrams_generated.keys():
-        print str(key) + ": " + str(ngrams_generated[key])
+    corpus = get_corpus(db_session)
+    tokens = tokenize(corpus)
+    model = build_model(tokens, 2)
+    sentence = generate(model, 2, max_iterations=20)
+    print ' '.join(sentence)
+    
 
 if __name__ == '__main__':
     main()
